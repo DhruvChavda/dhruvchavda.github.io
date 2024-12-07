7407d99a302c08fe72ad2b9336370ff77ab29665
Dhruv Chavda <dhruv.chavda@mindtickle.com>
2024-12-07 14:16:21 +0530

    chore: format files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dhruvchavda.github.io",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
