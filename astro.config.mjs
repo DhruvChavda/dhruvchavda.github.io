7807fc97f8ecb157dfa12f794149c7bb91353b55
Dhruv Chavda <dhruvchavda@Dhruvs-MacBook-Pro.local>
2026-02-08 18:27:53 +0530

    chore: added pageinsights changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://dhruvchavda.github.io",
   integrations: [tailwind()],
+  vite: {
+    build: {
+      minify: "terser",
+    },
+  },
 });
